3a8cbd80c6259e299d22a101ed8f4ff615d02f8e
Ashar2shahid <Ashar2shahid@gmail.com>
2022-07-22 14:49:38 +0530

    update for rsk-testnet

--- deploy/1_deploy.js
+++ deploy/1_deploy.js
@@ -1,5 +1,4 @@
 const hre = require('hardhat');
-const api3OperationsDeploymentReferences = require('@api3/operations/chain/deployments/references.json');
 
 module.exports = async () => {
   const dapiServerAddress = '0x3A09C138609F4b944EB7B8F0730e1972C243B33a';
